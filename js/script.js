c1ebb8024a501ad856f47be52b61f36ad5f7556c
Leonardo Freire <leoaguiarfreire@gmail.com>
2023-05-08 20:44:15 -0300

    tilt.js apenas desktop

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ import initAnimaScroll from './modules/anima-scroll.js';
 import initScrollSuave from './modules/scroll-suave.js';
 import initAnimaNumeros from './modules/anima-numeros.js';
 import initFuncionamento from './modules/funcionamento.js';
-import initVanillaTilt from './modules/vanilla-tilt.js';
 
 initBtnMobile();
 initAnimaScroll();
