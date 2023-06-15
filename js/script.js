cb609795522b981ff842e9cbd096e562ec7e3ddc
Leonardo Freire <leoaguiarfreire@gmail.com>
2023-06-15 15:11:38 -0300

    mudanca fundo main

--- js/script.js
+++ js/script.js
@@ -1,13 +1,11 @@
 import initBtnMobile from './modules/btn-mobile.js';
 import initAnimaScroll from './modules/anima-scroll.js';
 import initScrollSuave from './modules/scroll-suave.js';
-import initAnimaNumeros from './modules/anima-numeros.js';
 import initFuncionamento from './modules/funcionamento.js';
 import initVanillaTilt from './modules/vanilla-tilt.js';
 
 initBtnMobile();
 initAnimaScroll();
 initScrollSuave();
-initAnimaNumeros();
 initFuncionamento();
 initVanillaTilt();
