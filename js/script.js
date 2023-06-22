02143013ee6f20ad09ce6debdfc0fbffa75db4eb
Leonardo Freire <leoaguiarfreire@gmail.com>
2023-06-22 19:28:06 -0300

    slider parceiros

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initAnimaScroll from './modules/anima-scroll.js';
 import initScrollSuave from './modules/scroll-suave.js';
 import initFuncionamento from './modules/funcionamento.js';
 import initVanillaTilt from './modules/vanilla-tilt.js';
+import initSwiper from './modules/swiper.js';
 
 initBtnMobile();
 initAnimaScroll();
 initScrollSuave();
 initFuncionamento();
 initVanillaTilt();
+initSwiper();
