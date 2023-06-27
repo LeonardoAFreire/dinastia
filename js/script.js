4be584520bc58ff36b16de53e97920c8ff5fca30
Leonardo Freire <leoaguiarfreire@gmail.com>
2023-06-27 19:18:58 -0300

    mudanca cards

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ import initBtnMobile from './modules/btn-mobile.js';
 import initAnimaScroll from './modules/anima-scroll.js';
 import initScrollSuave from './modules/scroll-suave.js';
 import initFuncionamento from './modules/funcionamento.js';
-import initVanillaTilt from './modules/vanilla-tilt.js';
 import initSwiper from './modules/swiper.js';
 import initFormulario from './modules/formulario.js';
 
@@ -10,6 +9,5 @@ initBtnMobile();
 initAnimaScroll();
 initScrollSuave();
 initFuncionamento();
-initVanillaTilt();
 initSwiper();
 initFormulario();
